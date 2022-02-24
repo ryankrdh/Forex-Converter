7a7c1bf91d6b7b839cbc9b1c4ddf565691022d23
ryankrdh <ryankrdh@gmail.com>
2022-02-23 23:32:17 -0800

    writing out test cases

---
+++ test_app.py
@@ -0,0 +1,2 @@
+from unittest import TestCase
+from app import show_forex_form, homepage
---
+++ test_currency.py
@@ -0,0 +1,4 @@
+from unittest import TestCase
+from currency import CurrencyCalculator
+
+currency_rate = CurrencyCalculator()
