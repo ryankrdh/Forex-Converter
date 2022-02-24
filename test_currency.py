5e61328715f8a50f97fb7fe4b10168cc863a4663
ryankrdh <ryankrdh@gmail.com>
2022-02-24 00:14:39 -0800

    writing out test cases

--- test_currency.py
+++ test_currency.py
@@ -1,11 +1,13 @@
+from locale import currency
 from unittest import TestCase
 from currency import CurrencyCalculator
 
 currency_rate = CurrencyCalculator()
 
-class CurrencyCalculatorUnittest(TestCase):
+class CurrencyTestCase(TestCase):
     """
     Unit tests for currency.py
     """
 
     def check_valid_input(self):
+        self.assertTrue(currency_rate.check_valid_input('USD', 'EUR', 100))
